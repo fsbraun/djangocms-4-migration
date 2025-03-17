4b1216239a590b777c1e70eb735f0f075a6f1955
Fabian Braun <fsbraun@gmx.de>
2025-03-17 18:49:48 +0100

    remove language_code

--- djangocms_4_migration/test.py
+++ djangocms_4_migration/test.py
@@ -45,8 +45,8 @@ def test_pageurl_migration():
 
     page_urls = PageUrl.objects.order_by("language").all()
     assert page_urls.count() == 2, f"Expected 2 page urls, got {page_urls.count()}"
-    assert page_urls[0].language_code == "en", f"Expected language 'en', got {page_urls[0].language}"
-    assert page_urls[1].language_code == "fr", f"Expected language 'fr', got {page_urls[1].language}"
+    assert page_urls[0].language == "en", f"Expected language 'en', got {page_urls[0].language}"
+    assert page_urls[1].language == "fr", f"Expected language 'fr', got {page_urls[1].language}"
 
 
 def test_permissions_migration():
