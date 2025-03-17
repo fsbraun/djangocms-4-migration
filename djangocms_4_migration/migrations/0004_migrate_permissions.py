32533b72d235f1aa416cd0e0f113ab73cf86e486
Fabian Braun <fsbraun@gmx.de>
2025-03-17 17:05:14 +0100

    Modify perm instead of creating new one

--- djangocms_4_migration/migrations/0004_migrate_permissions.py
+++ djangocms_4_migration/migrations/0004_migrate_permissions.py
@@ -8,15 +8,13 @@ def forwards(apps, schema_editor):
     Permission = apps.get_model('auth', 'Permission')
 
     # Get all permissions related to the Title model
-    title_permissions = Permission.objects.filter(content_type__app_label='cms', content_type__model='title')
+    title_permissions = Permission.objects.filter(content_type__app_label='cms', content_type__model='pagecontent')
 
     for perm in title_permissions:
+        perm.codename.replace('_title', '_pagecontent')
+        perm.name.replace(' title', ' pagecontent')
+        perm.save()
         # Create the same permission for the PageContent model
-        new_perm, _ = Permission.objects.get_or_create(
-            codename=perm.codename.replace('_title', '_pagecontent'),
-            name=perm.name.replace(' title', ' pagecontent'),
-            content_type=PageContent._meta.get_field('content_type').related_model.objects.get(model='pagecontent')
-        )
         version_perm, _ = Permission.objects.get_or_create(
             codename=perm.codename.replace('_title', '_pagecontentversion'),
             name=perm.name.replace(' title', ' pagecontentversion'),
@@ -25,14 +23,10 @@ def forwards(apps, schema_editor):
 
         # Assign the new permission to users and groups
         for user in User.objects.filter(user_permissions=perm):
-            user.user_permissions.add(new_perm)
             user.user_permissions.add(version_perm)
 
         for group in Group.objects.filter(permissions=perm):
-            group.permissions.add(new_perm)
             group.permissions.add(version_perm)
-
-        perm.detele()
 
 
 class Migration(migrations.Migration):
